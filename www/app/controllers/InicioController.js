4e7524242c24b35915c27a1776327433f2f23ec4
joel <joel.r.mz@gmail.com>
2015-03-24 09:15:42 -0300

    Correcoes fake e texto

--- www/app/controllers/InicioController.js
+++ www/app/controllers/InicioController.js
@@ -24,59 +24,64 @@ angular.module('app.controllers',["ngCordova"])
     
     var registros = RegistroService.getRegistrosHojeFake(pis);
     
-    var Turnos =[];
-    var Intervalos=[];
-    var entradaEmAberto;
+    // var Turnos =[];
+    // var Intervalos=[];
+    // var entradaEmAberto;
     
-    if(registros.length>0){
-        registros.forEach(function(h,index){
-            if(h.sentido==="Entrada"&&index!==0){
-                var diff = h.data.diff(registros[index-1].data);
-                var a = moment.duration(diff);
-                var diff = moment.utc(diff);
-                var intervalo = {
-                    flotHrsIntervalo : a.asHours(),
-                    dif: diff,
-                    horasIntervaloStr :diff.format("HH:mm")
-                }
-                Intervalos.push(intervalo);
+    // if(registros.length>0){
+    //     registros.forEach(function(h,index){
+    //         if(h.sentido==="Entrada"&&index!==0){
+    //             var diff = h.data.diff(registros[index-1].data);
+    //             var a = moment.duration(diff);
+    //             var diff = moment.utc(diff);
+    //             var intervalo = {
+    //                 flotHrsIntervalo : a.asHours(),
+    //                 dif: diff,
+    //                 horasIntervaloStr :diff.format("HH:mm")
+    //             }
+    //             Intervalos.push(intervalo);
 
-            }
-            if(h.sentido ==="Saida"){            
-                var diff = h.data.diff(registros[index-1].data);
-                var a = moment.duration(diff);
-                var diff = moment.utc(diff);
-                var turno = {
-                    flotHrsTrabalhadas : a.asHours(),
-                    dif:diff,
-                    horasTrabalhadasStr : diff.format(":mm")
-                }
-                Turnos.push(turno);            
-            }
+    //         }
+    //         if(h.sentido ==="Saida"){            
+    //             var diff = h.data.diff(registros[index-1].data);
+    //             var a = moment.duration(diff);
+    //             var diff = moment.utc(diff);
+    //             var turno = {
+    //                 flotHrsTrabalhadas : a.asHours(),
+    //                 dif:diff,
+    //                 horasTrabalhadasStr : diff.format(":mm")
+    //             }
+    //             Turnos.push(turno);            
+    //         }
 
-            if(h.sentido ==="Entrada"&&!registros[index+1]){
-                entradaEmAberto = h;
-            }
-        });
-    }
-    
-    var totalTrabalhando;
-    if(entradaEmAberto){
-        var now = moment();
-        var difNow = now.diff(entradaEmAberto.data);
-        var trabalhando = moment.utc(difNow);
-        totalTrabalhando = trabalhando;
+    //         if(h.sentido ==="Entrada"&&!registros[index+1]){
+    //             entradaEmAberto = h;
+    //         }
+    //     });
+    // }
+    // var totalTrabalhando;
+    // if(entradaEmAberto){
+    //     var now = moment();
+    //     var difNow = now.diff(entradaEmAberto.data);
+    //     var trabalhando = moment.utc(difNow);
+    //     totalTrabalhando = trabalhando;
         
-    }
-    var totalTrabalhado=0;
-    var totalIntervalos =0;
-    Turnos.forEach(function(t){
-        totalTrabalhado+=t.dif;
-    });
-    Intervalos.forEach(function(i){
-        totalIntervalos+=i.dif;
-    });
+    // }
+    // var totalTrabalhado=0;
+    // var totalIntervalos =0;
+    // Turnos.forEach(function(t){
+    //     totalTrabalhado+=t.dif;
+    // });
+    // Intervalos.forEach(function(i){
+    //     totalIntervalos+=i.dif;
+    // });
+    
+    var totalIntervalos = RegistroService.getIntervalo(registros);
+    var totalTrabalhando = RegistroService.getTurnoAtual(registros);
+    var totalTrabalhado =RegistroService.getTotalTrabalhado(registros);
+   
     var momentTrabalhadas = totalTrabalhado > 0? moment.utc(totalTrabalhado): totalTrabalhando;
+    
     var momentIntervalos = totalIntervalos > 0? moment.utc(totalIntervalos):{};
     var proximaQuebra;
     
@@ -84,12 +89,15 @@ angular.module('app.controllers',["ngCordova"])
         proximaQuebra= {Hora : moment(data.Hora,["YYYY-MM-DD HH:mm:ss"]),Mensagem: data.Mensagem};
     });
     proximaQuebra = RegistroService.getProximaQuebraFake();
-    momentTrabalhadas = momentTrabalhadas.add(totalTrabalhando.minute(),"minute").add(totalTrabalhando.hour(),"hour")
-    var teste = proximaQuebra.Hora.diff(now);
-    proximaQuebra.Hora = moment.utc(teste);
+   
+    momentTrabalhadas = momentTrabalhadas.add(totalTrabalhando?totalTrabalhando.minute():0,"minute").add(totalTrabalhando?totalTrabalhando.hour():0,"hour")
+    proximaQuebra.Hora = moment.utc(proximaQuebra.Hora.diff(moment()));
+    
+    debugger;
+    var testeIntervalo = RegistroService.getIntervalo(registros);
+    var testeTurno = RegistroService.getTurnoAtual(registros);
     $scope.dadosInicio ={totalTrabalhado:momentTrabalhadas,
                          totalIntarvalos:momentIntervalos,
-//                         horasDoTurno:totalTrabalhando,
                          proximaQuebra:proximaQuebra};
   
 })
--- www/app/services/registroService.js
+++ www/app/services/registroService.js
@@ -4,7 +4,12 @@ angular.module('app.services', [])
 */
 
 .factory("RegistroService",function($http){
-       
+    
+    var DiferencaDatas = function(dataA,dataB){
+                            var diff = dataA.diff(dataB);
+                            return moment.utc(diff);
+                    };
+    
     return{
         
         getRegistrosHoje: function(pis){
@@ -19,6 +24,15 @@ angular.module('app.services', [])
             var url="http://fortesponto.azurewebsites.net/api/Regras/"+pis+"/ProximaQuebra";
                 return $http.get(url);                    
         },
+        
+        getInitialData: function(registros){
+
+                    
+
+                    return {totalTrabalhado:momentTrabalhadas,totalIntervalos:momentIntervalos}
+                    
+        },
+
         getProximaQuebraFake: function(){
             var retorno={};
             
@@ -31,31 +45,61 @@ angular.module('app.services', [])
             return retorno;
             
         },
-     
-        
+        getTotalTrabalhado:function(registros){
+            var result=0;
+            if (registros && registros.length>0) {
+                registros.forEach(function(dado,index){
+                    if(dado.sentido==="Saida")
+                        result+=(DiferencaDatas(dado.data,registros[index-1].data))
+                    if(dado.sentido==="Entrada" && !registros[index+1])
+                        result += (DiferencaDatas(dado.data,moment()));
+                });
+            };
+
+
+        },
+        getIntervalo  :function(registros){
+            var result =0;
+            if(registros && registros.length >0){
+                registros.forEach(function(dado , index){
+                    if(dado.sentido === "Entrada"&& index>0)
+                        result+=DiferencaDatas(dado.data,registros[index-1].data);
+                });
+             }
+             return moment.utc(result);
+        },
+        getTurnoAtual:function(registros){
+            var result;
+            if(registros && registros.length>0)
+                registros.forEach(function(dado,index){
+                    if(dado.sentido==="Entrada" && !registros[index+1])
+                        result = dado.data;
+                });
+            if(result)
+                return  moment.utc(DiferencaDatas(moment(),result));
+            else
+                return null;
+        },
         getRegistrosHojeFake:function(){            
             
             
             var retorno = [];
-            if(moment().hour()>=8)
             retorno.push({  sentido:"Entrada",
                             data: moment("08:00",["HH:mm"]),
                             hora:  moment("08:00",["HH:mm"]).format("HH:mm")});
-            if(moment().hour()>=12)
            retorno.push({  sentido:"Saida",
                            data: moment("12:00",["HH:mm"]),
                            hora:  moment("12:00",["HH:mm"]).format("HH:mm")});
-            if(moment().hour()>=13)
             retorno.push({  sentido:"Entrada",
                             data: moment("13:00",["HH:mm"]),
                             hora:  moment("13:00",["HH:mm"]).format("HH:mm")});
-            if(moment().hour()>=17)
-            retorno.push({  sentido:"Saida",
-                            data: moment("17:00",["HH:mm"]),
-                            hora:  moment("17:00",["HH:mm"]).format("HH:mm")});
+            // retorno.push({  sentido:"Saida",
+            //                 data: moment("17:00",["HH:mm"]),
+            //                 hora:  moment("17:00",["HH:mm"]).format("HH:mm")});
                           
             return retorno;
         }
+
     };
     
 });
